8b3bd76ff3e3444a8c6fa37e63f377424414e01c
kjndfahu <denispoda59@gmail.com>
2025-06-22 12:44:48 +0300

    new

--- src/features/product-page/ui/price-block.tsx
+++ src/features/product-page/ui/price-block.tsx
@@ -55,13 +55,21 @@ export const PriceBlock = ({
         <div className="flex flex-col bg-[#F5F7FF] rounded-[20px] gap-[20px] md:p-6 p-4">
             <div className="flex flex-col items-start gap-2">
                 <div className="flex items-baseline gap-2">
-                    <h4 className="md:text-[36px] md:leading-[40px] text-[27px] leading-[30px] font-semibold text-[#161616]">
-                        {discountedNum.toLocaleString("ru-RU")}<span className="text-[18px] font-medium">₽</span>
-                    </h4>
-                    <div className="rounded-full px-[6px] font-semibold bg-[#FEECEE] py-1 text-[11px] text-[#E71730]">
-                        -{discountPercentage}%
-                    </div>
-                    <span className="text-[16px] line-through text-gray-500">/ {regularNum.toLocaleString("ru-RU")}₽</span>
+                    {regularNum !== discountedNum ? (
+                        <>
+                            <h4 className="md:text-[36px] md:leading-[40px] text-[27px] leading-[30px] font-semibold text-[#161616]">
+                                {discountedNum.toLocaleString("ru-RU")}<span className="text-[18px] font-medium">₽</span>
+                            </h4>
+                            <div className="rounded-full px-[6px] font-semibold bg-[#FEECEE] py-1 text-[11px] text-[#E71730]">
+                                -{discountPercentage}%
+                            </div>
+                            <span className="text-[16px] line-through text-gray-500">/ {regularNum.toLocaleString("ru-RU")}₽</span>
+                        </>
+                    ) : (
+                        <h4 className="md:text-[36px] md:leading-[40px] text-[27px] leading-[30px] font-semibold text-[#161616]">
+                            {discountedNum.toLocaleString("ru-RU")}<span className="text-[18px] font-medium">₽</span>
+                        </h4>
+                    )}
                 </div>
             </div>
             <div className="flex flex-col w-full items-center self-center gap-[12px]">
